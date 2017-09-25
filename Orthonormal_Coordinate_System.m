56080ad983772c9c10eee00c8009ba91fe3a66dd
Ese Okonofua <eseokonofua95@gmail.com>
2017-09-25 17:38:40 -0400

    Orth coord sys

---
+++ Orthonormal_Coordinate_System.m
@@ -0,0 +1,6 @@
+## Author: Eseoghene Okonofua <EseO@Eseoghenes-MacBook-Pro.local>
+## Created: 2017-09-25
+
+function [Base1, Base2, Base3, Centre] = Orthonormal_Coordinate_System (point1, point2, point3)
+
+endfunction
